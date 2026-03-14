438d6f030cf0ab7066917ddf2a2518ba7031e10d
chethankumar <chethankumarchikkahongaiah@gmail.com>
2026-03-14 20:49:12 +0530

    adding VPC module with AZ

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -1,134 +1,143 @@
-# VPC
+locals {
+  subnet_cidrs = cidrsubnets(aws_vpc.my-vpc.cidr_block, 8, 8, 8, 8)
+  public_subnets = {
+    public_subnet_1 = {
+        cidr = local.subnet_cidrs[0]
+        az = "ap-south-2a"
+    }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
-
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
-  tags = {
-    Name = "${var.project_name}-vpc"
-  } 
-}
-
-# Subnets
-
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.public_subnet_cidr_block
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.project_name}-public-subnet"
+    public_subnet_2 = {
+        cidr = local.subnet_cidrs[1]
+        az = "ap-south-2b"
+    }
   }
 
-}
+  private_subnets = {
+    private_subnet_1 = {
+        cidr = local.subnet_cidrs[2]
+        az = "ap-south-2a"
+    }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.private_subnet_cidr_block
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = "-private-subnet"
-  }
-
-}
-# Internet Gateway
-
-resource "aws_internet_gateway" "my_internet_gateway" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "${var.project_name}-igw"
+    private_subnet_2 = {
+        cidr = local.subnet_cidrs[3]
+        az = "ap-south-2b"
+    }
   }
 }
 
-#elastic ip
+#VPC
+resource "aws_vpc" "my-vpc" {
+    cidr_block = var.cidr_block
+
+    enable_dns_support = true
+    enable_dns_hostnames = true
+  
+}
+
+# Public subnet
+
+resource "aws_subnet" "Public" {
+    for_each = local.public_subnets
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = each.value.cidr
+    availability_zone = each.value.az
+
+    tags = {
+      Name = var.project_name
+    }
+}
+
+#Private Subnet
+
+resource "aws_subnet" "private" {
+    for_each = local.private_subnets
+    vpc_id = aws_vpc.my-vpc.id
+    cidr_block = each.value.cidr
+    availability_zone = each.value.az
+
+    tags = {
+        Name = var.project_name
+    }
+}
+
+
+#Internet GateWay
+
+resource "aws_internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my-vpc.id
+
+    tags = {
+        Name = var.project_name
+    }
+}
+
+
+#Nat Gateway
 
 resource "aws_eip" "my_eip" {
-  domain = "vpc"
+    domain = "vpc"
 }
 
-# nat gateway
+resource "aws_nat_gateway" "my_nat_gw" {
+    allocation_id = aws_eip.my_eip.id
+    subnet_id = aws_subnet.Public.id
 
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.my_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
-
-  tags = {
-    Name = "demo-nat-gateway"
-  }
-  
+    tags = {
+        Name = var.project_name
+    }
 }
 
 # Route Table
-
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_internet_gateway.id
-  }
-
-  tags = {
-    Name = "${var.project_name}-public-rt"
-  }
-  
+    vpc_id = aws_vpc.my-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_igw.vpc_id
+    }
 }
 
 resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_internet_gateway.id
-  }
-
-  tags = {
-    Name = "demo-private-rt"
-  }
-  
+    vpc_id = aws_vpc.my-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_nat_gateway.my_nat_gw.id
+    }  
 }
 
-# Route Table Associations
-
-resource "aws_route_table_association" "public_subnet_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_route_table_association" "public_RTA" {
+    subnet_id = aws_subnet.private.id
+    route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "private_subnet_association" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_route_table.id
+resource "aws_route_table_association" "private_RTA" {
+    subnet_id = aws_subnet.private.id
+    route_table_id = aws_route_table.private_route_table.id
 }
 
-#security group
-resource "aws_security_group" "my_security_group" {
-  name        = "${var.vpc_name}-sg"
-  description = "Security group for ${var.vpc_name}"
-  vpc_id      = aws_vpc.my_vpc.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+# Security Group
+resource "aws_security_group" "vpc_sg" {
+    vpc_id = aws_vpc.my-vpc.id
+    name = var.aws_security_group_name
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "HTTP"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0   .0.0.0/0"]
+        from_port = 443
+        to_port = 443
+        protocol = "HTTPS"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
--- module/vpc/variables.tf
+++ module/vpc/variables.tf
@@ -1,39 +1,17 @@
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default = "10.0.0.0/16"
-}
-
-variable "vpc_name" {
-  description = "The name of the VPC"
-  type        = string
-  default = "my-vpc"
-}
-
-variable "project_name" {
-  description = "The name of the project"
-  type        = string
-  default = "my-project"
-}
-
-variable "public_subnet_cidr_block" {
-  description = "The CIDR block for the public subnet"
-  type        = string
-  default = "10.0.1.0/24"
+variable "cidr_block" {
+    type = string
+    description = "CIDR block required for VPC" 
   
 }
 
-variable "availability_zone" {
-  description = "The availability zone for the public subnet"
-  type        = string
-  default = "us-east-1a"
+variable "project_name" {
+    type = string
+    description = "Project name"
+  
 }
 
-variable "private_subnet_cidr_block" {
-  description = "The CIDR block for the private subnet"
-  type        = string
-  default = "10.0.2.0/24"
+variable "aws_security_group_name" {
+    type = string
+    description = "VPC SG name"
+  
 }
-
-
-
